c87fc54ded18b6b002df5206f736eb984c863e8d
Panom <panompreston@gmail.com>
2025-05-28 12:48:49 -0700

    made first screen and connected it to Main

--- src/main/java/OrderScreen.java
+++ src/main/java/OrderScreen.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
+
 public class OrderScreen {
 
+    static Scanner scanner = new Scanner(System.in);
+
+    public void Start() {
+        //while loop that stops when "choice" is false
+        boolean choice = true;
+        while (choice) {
+            //home screen
+            System.out.println(""" 
+                    🥪Welcome to DelI-cious🥪
+                    1) New Order
+                    0) Exit
+                    """);
+            System.out.print("Enter: ");
+            String enter = scanner.nextLine(); // input of what the user wants to do
+            //switch for the 2 choices and default is "invalid input"
+            switch (enter) {
+                case "1":
+                    break;
+                case "0":
+                    System.out.println("Thanks for using the app \uD83D\uDC3B");
+                    choice = false;
+                    break;
+                default:
+                    System.out.println("X invalid input X");
+            }
+
+        }
+
+    }
 }
